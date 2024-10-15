7e49851d47cf342a3a8ba150c3bdbf791e28c6b9
zuran <zuran@shubham>
2024-10-16 01:47:36 +0530

    Multiple class with sub

--- src/hello/dell.java
+++ src/hello/dell.java
@@ -1,9 +1,33 @@
 package hello;
+import java.util.Scanner;
 
-public class dell {
+class hp
+{
+	int x,y,z;
+	void getvalue(int a,int b)
+	{
+		x=a;
+		y=b;
+	}
+	void add()
+	{
+		z=x+y;
+		System.out.println(z);
+		
+	}
+}
+public class dell{
 
-	public static void main(String[] args) {
-System.out.println("2nd progream");
+	public static void main(String[] args) 
+	{
+		 Scanner s=new Scanner(System.in);
+		 int v1=s.nextInt();
+         System.out.println("2nd progream");
+         int v2=s.nextInt();
+         hp ll=new hp();
+         ll.getvalue(v1, v2);
+         ll.add();
+        
 	}
 
 }
